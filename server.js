ade70188683966f9aa336f9f909e423d2d34224f
0800 <spmspy0800@gmail.com>
2026-04-09 16:01:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -91,6 +91,8 @@ app.get("/screenshot", async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT)
 })
